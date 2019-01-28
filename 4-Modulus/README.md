fc4d0f729a10ffe0bd72d3d8872476e341a70f16
Gilles Blanchard <neworldwebsites@gmail.com>
2019-01-28 09:25:55 +0100

    ras

---
+++ 4-Modulus/README.md
@@ -0,0 +1,19 @@
+# Objective
+
+    Write a function that if a number 3 or greater and is divisable by 3 returns 'fizz', by 5 returns 'buzz', by both returns 'fizzbuzz', or none ''.
+
+## Example Returns
+
+* yourFunction(3) will return 'fizz'
+
+* yourFunction(5) will return 'buzz'
+
+* yourFunction(4) will return ''
+
+* yourFunction(15) will return 'fizzbuzz'
+
+## How i solved it Hints
+
+* 1 parameter
+* if, else if, else
+* % operator
---
+++ 4-Modulus/modulus.js
@@ -0,0 +1,36 @@
+// SOLUTION 1
+function myFunction(num = int) {
+	// start by the most exigeant condition
+	if (num % 3 === 0 && num % 5 === 0) {
+		return 'fizzbuzz';
+	} 
+	else if (num % 3 === 0) {
+		return 'fizz';
+	}
+	else if (num % 5 === 0) {
+		return 'buzz';
+	}
+		return '';
+} 
+console.log(myFunction(3));
+console.log(myFunction(5));
+console.log(myFunction(4));
+console.log(myFunction(15));
+
+
+// SOLUTION 2
+function myFunction2(num = int) {
+	let word = '';
+	// for 15 both will be concat to word
+	if (num % 3 === 0) {
+		word = word.concat('fizz');
+	}
+	if (num % 5 === 0) {
+		word = word.concat('buzz');
+	} 
+	return word;
+}
+console.log(myFunction2(3));
+console.log(myFunction2(5));
+console.log(myFunction2(4));
+console.log(myFunction2(15));
