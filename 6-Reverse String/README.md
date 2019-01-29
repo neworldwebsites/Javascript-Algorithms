c5e35f27e14d54cd94213ca65b1cc9a997a9af22
Gilles Blanchard <neworldwebsites@gmail.com>
2019-01-29 09:48:36 +0100

    ras

---
+++ 6-Reverse String/README.md
@@ -0,0 +1,17 @@
+# Objective
+
+    Write a function that takes in a string and reverses its value.
+
+## Example Returns
+
+* yourFunction('codegod') will return '**dogedoc**'
+
+* yourFunction('javascript') will return **'tpircsavaj'**
+
+## How i solved it Hints
+
+* 1 parameter
+* reverse()
+* split()
+* join()
+* for loop
---
+++ 6-Reverse String/reverse.js
@@ -0,0 +1,23 @@
+// SOLUTION 1
+function myFunction(word) {
+	// 3 iterations !
+	let res = word.split("").reverse().join("");
+
+	return res;
+} 
+console.log(myFunction('codegod'));
+console.log(myFunction('javascript'));
+
+
+// SOLUTION 2
+function myFunction2(word) {
+	// 1 iteration only !
+	let res = '';
+	for (let i = word.length - 1; i >= 0; i--){
+		// res = res.concat(word[i]);		
+		res += word[i];
+	}
+	return res;
+}
+console.log(myFunction2('codegod'));
+console.log(myFunction2('javascript'));
