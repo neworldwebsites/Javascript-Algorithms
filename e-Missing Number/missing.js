c400d8f3b9dfd92cc792d6599db817f712cf2070
Gilles Blanchard <neworldwebsites@gmail.com>
2019-01-30 05:33:35 +0100

    ras

--- e-Missing Number/missing.js
+++ e-Missing Number/missing.js
@@ -1,8 +1,18 @@
-function myFunction(num) { 
+function myFunction(numArr) { 
 
+	numArr = numArr.sort((num1, num2) => {
+		return num1 - num2;
+	});
 
-	
+	for (let i = 1; i < numArr.length; i++){
+		if (i != numArr[i]) {
+			return i;
+		}
+		
 	}
+
+	return numArr;
+}
 console.log(myFunction([0, 3, 5, 8, 4, 6, 1, 9, 7])); // 2
 console.log(myFunction([1, 2, 5, 0, 6, 7, 9, 3, 4])); // 8
 
--- f-Duplicate Numbers/duplicate.js
+++ f-Duplicate Numbers/duplicate.js
@@ -1,7 +1,16 @@
-function myFunction(num) {
+function myFunction(numArr) {
 
+	numArr = numArr.sort((num1,num2) => {
+		return num1 < num2;
+	});
 
-
+	for (let i = 1; i < numArr.length; i++){
+		if (numArr[i] === numArr[i + 1]) {
+			return true;
+		}
+	}
+	// coz numArr is sorting then if next = i, it's duplicate
+	return false;
 }
 console.log(myFunction([1, 2, 3, 1])); // true
 console.log(myFunction([3, 1])); // false
