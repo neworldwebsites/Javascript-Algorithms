fc582654dac84aba1bb0d591630b7a5d70d751c9
Gilles Blanchard <neworldwebsites@gmail.com>
2019-01-30 02:17:58 +0100

    ras

---
+++ c-Longest Word/README.md
@@ -0,0 +1,20 @@
+# Objective
+
+    Write a function that takes in a string and returns the largest word in a array.
+
+## Example Returns
+
+* yourFunction('Ready, steady, go!') will return **'steady'**
+
+* yourFunction('Ready[[[, steady, go!') will return **'steady'**
+
+* yourFunction('ABCd') will return **'ABCd'**
+
+## How i solved it Hints
+
+* replace()
+* split()
+* for loop
+
+
+
---
+++ c-Longest Word/word.js
@@ -0,0 +1,16 @@
+function myFunction(text) { 
+	let res = text.replace(/[\W_]/g, " ").split(' ');
+	let largest = "";
+
+	res.forEach((word) => {
+
+		largest = word.length > largest.length ? word : largest;
+
+	});
+	return largest;
+	}
+console.log(myFunction('Ready, steady, go!')); // coding
+console.log(myFunction('Ready[[[, steady, go!')); // steady
+console.log(myFunction('ABCd')); // ABCd
+
+
